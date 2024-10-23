19727783c87cdd6ca25084e434b7b646de70c1ff
davidaliri <davidaliri95@gmail.com>
2024-10-23 14:42:36 -0500

    Add files via upload

---
+++ basedepreguntas.js
@@ -0,0 +1,194 @@
+basedepreguntas = [
+    {
+
+      pregunta: `TEXTO NO 1`,
+      imagen: `https://i.ibb.co/M67VFJn/Whats-App-Image-2024-10-23-at-11-23-45-AM.jpg`,
+      respuesta: `	`,
+      distractores: [`	`, `	`, ``],
+      
+      
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `1. En el texto, ¿cuáles son los rasgos que se destacan en el personaje principal?`,
+      respuesta: `		Docilidad, bondad y ternura.`,
+      distractores: [`	Impaciencia y temor a los animales.`, `Pulcritud y paciencia.`, `	Ambición y determinación.`],
+      ayuda: ``,
+      ayudaImg: ``,
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `2.	En el párrafo tres aparece la palabra METAMORFOSEADAS, ¿qué significa la palabra metamorfoseadas en el contexto del texto?`,
+      respuesta: `Ha cambiado.`,
+      distractores: [`	Tomar posición.`, `Ocasiona miedo`, `	Permanece igual`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `	3.	¿De qué trata principalmente el texto?.`,
+      respuesta: `		El amor del protagonista por los animales y el apego que tenía a su gato.`,
+      distractores: [`		La alegría que el protagonista compartía con su esposa y la naturaleza`, `	La relación entre el protagonista y su esposa por compartir sus preferencias`, `		La descripción detallada de la casa del protagonista principal de la historia`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `4.	Lee el siguiente fragmento del texto nuevamente :...."  Plutón — tal era el nombre del qato— se había convertido en mi favorito y mi camarada. Sólo yo le daba de comer y él me seguía por todas partes en casa. Me costaba mucho impedir que anduviera tras de mí en la calle. "  ¿Por qué el autor utiliza las rayas en el texto?`,
+      respuesta: `	 Considero que es para encerrar aclaraciones o incisos en el texto.`,
+      distractores: [`		Parece que quiere destacar al personaje secundario de la historia.`, `		Imagino que desea indicar el suspenso en la historia.`, `		Pienso que quiere enmarcar la reproducción de una cita textual.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/JFp7wJL/Whats-App-Image-2024-10-23-at-12-16-52-PM.jpg` ,
+      pregunta: `TEXTO Nº 2`,
+      respuesta: `	`,
+      distractores: [`	`, `	`, `	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `5. Según las investigaciones realizadas sobre el tema, ¿cuál es la consecuencia del baile?.`,
+      respuesta: `		Aumenta la creatividad y la autoestima`,
+      distractores: [`		Tiene una gran capacidad sanadora`, `		Aleja las tensiones y preocupaciones`, `		Tiene capacidad de producción de hormonas`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: ` 6.	¿De qué trata principalmente el texto leído?.`,
+      respuesta: `	b.	La actividad del baile es un remedio contra el estrés y mal humor`,
+      distractores: [`	La melodía interna que la mente reconoce como una partitura`, `		La conexión con la esencia más íntima de la persona`, `La persona se siente más relajada, receptiva y llena de energía`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `7.	Lee el fragmento del texto: Se ha demostrado que el baile es uno de los mejores antídotos contra el estrés y el mal humor. No en vano es un ran estimulante en la roducción de endorfinas, las hormonas del bienestar.   ---------------------    -------------------------- -----------¿Qué significa la palabra antídoto?`,
+      respuesta: `d.	Es una medicina que cura al estrés y el mal humor`,
+      distractores: [`		Es un estimulante para la producción de endorfina`, `		Es una sustancia que daña a la persona`, `		Es una alternativa para las enfermedades`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `8.	¿Con qué finalidad se afirma que todos tenemos una melodía interna?`,
+      respuesta: `	Señalar que todos podemos bailar, sin ser expertos`,
+      distractores: [`		Demostrar que la música tiene melodías que nos ayuda`, `	Indicar que no todos tenemos la habilidad de bailar.`, `		Invitar a practicar constantemente para ser expertos`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/T46N30f/Whats-App-Image-2024-10-23-at-1-00-08-PM-1.jpg` ,
+      pregunta: `TEXTO Nº 3`,
+      respuesta: `	`,
+      distractores: [`	`, ``, `		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `9.	Según el texto leído, ¿qué está prohibido en voleibol?`,
+      respuesta: `	Tocar la red o net`,
+      distractores: [`		Tres golpes por equipo.`, `		Saltar más de 2.44 m.`, `			Rotar en cada saque.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `	10.	¿Qué diferencia hay entre la zona de ataque y defensa de la cancha?`,
+      respuesta: `		La zona alejada es para recepcionar y la zona cercana a la red, para pasar la pelota.`,
+      distractores: [`		Las dos partes de la cancha tienen diferentes diseños según los requerimientos del juego.`, `		Los dos equipos de voleibol están compuestos por tres defensas, dos zagueros y un líbero.`, `	Las dos zonas de la cancha de voleibol se utilizan para pasar y recepcionar la pelota.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `	1 1. ¿Cuál es la finalidad de la infografía leída?`,
+      respuesta: `		Instruir cómo se debe jugar el voleibol.`,
+      distractores: [`			Sustentar las reglas de juego de voleibol.`, `		Describir las medidas de la cancha de voleibol`, `			Exponer las prohibiciones en el voleibol.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `	12. ¿Qué opinas sobre las reglas del voleibol del texto leído?.`,
+      respuesta: `			Son buenas, porque permite un adecuado desarrollo del juego y evitar reclamos.`,
+      distractores: [`		Son poco claras, porque las jugadoras tienen su interpretación de cada regla.`, `			Son muy pocas, ya que en un juego real puede ocasionar reclamos de las jugadoras.`, `		Son de manejo para el árbitro, quien debe conocer todas las reglas de la infografía.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/zNKzjs8/Whats-App-Image-2024-10-23-at-1-28-56-PM.jpg` ,
+      pregunta: `TEXTO Nº 4`,
+      respuesta: `	`,
+      distractores: [``, ``, ``],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `13. ¿Qué es vital para sobrevivir a las turbulencias de la adolescencia?`,
+      respuesta: `			Mantener una buena comunicación entre padres e hijos.`,
+      distractores: [`		Tener nuevas experiencias de ilusiones y emociones.`, `		Tomar en cuenta que es un período de grandes contrastes.`, `		Pasar por las etapas de ilusiones y emociones.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `	14. ¿Cuál es la intención del autor del texto leído?`,
+      respuesta: `			Explicar las características del primer amor del adolescente`,
+      distractores: [`		Convencer a los padres y madres para orientarles a sus hijos.`, `			Advertir los primeros conflictos con padres y/o madres e hijos.`, `		Recomendar a padres y/o madres e hijos una buena comunicación.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `15. ¿Por qué los padres se preocupan ante el primer amor de sus hijos adolescentes?.`,
+      respuesta: `			Es una etapa de inestabilidad.`,
+      distractores: [`		Experimentan las sensaciones.`, `			Es el amor que no se olvida.`, `	Pasan más tiempo con la pareja.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `16. ¿Qué opinas sobre la oportunidad de los padres para tener una conversación ante el prime amor que experimentan sus hijos?`,
+      respuesta: `		Estoy de acuerdo porque permite a los padres orientarles y aclarar a sus hijos.`,
+      distractores: [`		No me parece que haya más acercamiento y demostración de afecto a sus parejas.`, `	Me parece bien que sienta rabia, enojo e impotencia que antes no había tenido`, `		Estoy de acuerdo que el primer amor representa un cambio significativo en la vida.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/N7LqnPr/Whats-App-Image-2024-10-23-at-1-59-29-PM.jpg` ,
+      pregunta: `TEXTO Nº 5`,
+      respuesta: `			`,
+      distractores: [`			`, `	`, `	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `17.	¿Cuál es objeto que era símbolo de rango?.`,
+      respuesta: `				Una sonaja de oro.`,
+      distractores: [`		El cuchillo de cobre..`, `			La insignia de oro.`, `	Unas campanas de cobre.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `18.	Según los objetos encontrados en la tumba, ¿cuál es la alternativa que expresa la característica implícita del gran Señor Moche?.`,
+      respuesta: `				Trascendental.`,
+      distractores: [`		Sabio`, `			Astuto..`, `		Justo.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `19. En el texto aparece la siguiente expresión: "Además, sobre el cadáver, y en sucesivas capas, aparecen abanicos y tocados de plumas, once chaquiras de conchas marinas y cobre, una insignia de oro, la manta adornada con plaquetas y campanas de cobre y una camisa blanca. También ornamentos de oro, plata y cobre cubren el cráneo, la cara, el cuello, las orejas el pecho".   ________________________________________________________________________________________ ¿Qué significa la palabra ornamentos?`,
+      respuesta: `				Adornos.`,
+      distractores: [`			Pertenencias.`, `			Objetos.`, `		Piezas.`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `20. ¿Qué opinas sobre el hecho de enterrar al señor Moche con vestimenta y objetos de valor? .`,
+      respuesta: `				Opino que el señor Moche fue un personaje que tenía rango entre los mochicas.`,
+      distractores: [` Creo que los muertos en toda cultura se merecen el respeto y admiración.`, `		Me parece que todos los objetos encontrados son parte de la tradición moche.`, `		No me parece enterrar a alguien con objetos costosos por más que sean importantes.`],
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ];
